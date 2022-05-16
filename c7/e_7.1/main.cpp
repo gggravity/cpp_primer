bf22e75481251b597d11682d7089b321a0856575
martin <m@rtin.bz>
2022-05-16 18:04:08 -0200

    e 7.1

--- c7/e_7.1/main.cpp
+++ c7/e_7.1/main.cpp
@@ -1,41 +1,51 @@
 #include <iostream>
-#include "Sales_data.h"
+//#include "Sales_data.h"
 
 using namespace std;
 
-// 0-201-78655 5 98.50 0-201-78655 5 98.50
+// 0-201-78655 5 98.50
+// 0-201-78655 5 98.50
+
+struct Sales_data
+    {
+        string bookNo;
+        unsigned units_sold = 0;
+        double revenue = 0.0;
+    };
 
 int main ()
-{
+  {
 
-  Sales_data total;
-  double price;
+    Sales_data total;
+    double price;
 
-  if (cin >> total.bookNo >> total.units_sold >> price)
-    {
-      total.revenue = price * total.units_sold;
-      Sales_data trans;
-
-      while (cin >> trans.bookNo >> trans.units_sold >> price)
-        trans.revenue = price * trans.units_sold;
+    if (cin >> total.bookNo >> total.units_sold >> price)
       {
-        if (total.bookNo == trans.bookNo)
-          {
-            total.revenue += trans.revenue;
-          }
-        else
-          {
-            cout << "Book revenue: " << total.revenue << endl;
-            total.units_sold += trans.units_sold;
-            total.revenue += trans.revenue;
+        total.revenue = price * total.units_sold;
+        Sales_data trans;
 
+        while (cin >> trans.bookNo >> trans.units_sold >> price)
+          {
+            trans.revenue = price * trans.units_sold;
+            if (total.bookNo == trans.bookNo)
+              {
+                total.revenue += trans.revenue;
+                cout << "adding " << trans.revenue
+                     << " to old book revenue"
+                     << " for a total of: "
+                     << total.revenue << endl;
+              }
+            else
+              {
+                cout << "Book revenue: " << total.revenue  << " For: " << total.bookNo << endl;
+                total = trans;
+              }
           }
+        cout << "Total revenue: " << total.revenue << endl;
       }
-      cout << "Total revenue: " << total.revenue << endl;
-    }
-  else
-    {
-      cerr << "No data?!" << endl;
-    }
-  return 0;
-}
+    else
+      {
+        cerr << "No data?!" << endl;
+      }
+    return 0;
+  }
