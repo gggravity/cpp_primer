f348c7e0b46920f87f5ed505c26ef25acdeb65ea
martin <m@rtin.bz>
2022-05-21 21:26:27 -0200

    Section 10.6

---
+++ c10/section_10.6/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print (list<string> &list, const string& title)
+  {
+    cout << title << ": ";
+    for (auto &item : list)
+      {
+        cout << item << " ";
+      }
+    cout << endl;
+  }
+
+int main ()
+try
+  {
+    string s { "the quick red fox jumps over the slow red turtle" };
+    istringstream iss { s };
+    list<string> ls;
+
+    for (string word ; iss >> word ;)
+      {
+        ls.push_back(word);
+      }
+
+    print(ls, "list string");
+    ls.sort();
+    print(ls, "list sorted");
+    ls.unique();
+    print(ls, "dup removed");
+    
+    return 0;
+  }
+catch (exception &e)
+  {
+    cerr << "Error: " << e.what() << endl;
+  }
+
+
