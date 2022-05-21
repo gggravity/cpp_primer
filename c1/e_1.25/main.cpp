4c955b9f65b2b660974f394c21d2fc9241880319
martin <m@rtin.bz>
2022-05-21 20:14:41 -0200

    Section 10.4.2

---
+++ c1/e_1.25/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include "Sales_item.h"
+
+using namespace std;
+
+int main ()
+  {
+    Sales_item total;
+
+    if (cin >> total)
+      {
+        Sales_item trans;
+        {
+          while (cin >> trans)
+            {
+              if (total.isbn() == trans.isbn())
+                {
+                  total += trans;
+                }
+              else
+                {
+                  cout << total << endl;
+                  total = trans;
+                }
+            }
+        }
+      }
+    return 0;
+  }
