e2bce95ead651405167d5db0a228c23829b77073
martin <m@rtin.bz>
2022-05-21 16:20:40 -0200

    Section 10.3.3

---
+++ c10/section_10.3.3/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (int argc, char *argv[])
+try
+  {
+    // e 10.20
+    vector<string> words { "the", "quick", "red", "fox", "jumps", "over", "the", "slow", "red", "turtle" };
+    vector<string>::size_type sz { 5 };
+
+    auto count = count_if(words.begin(), words.end(), [sz] (const string &s)
+      {
+          return s.size() > sz;
+      });
+
+    cout << count << " are larger then " << sz << " characters." << endl;
+
+    // e 10.21
+
+    cout << boolalpha << [sz] () mutable -> bool
+      {
+          while (sz > 0)
+            {
+              --sz;
+              cout << sz << " ";
+            }
+            cout << endl;
+            return true;
+      }();
+    
+    return 0;
+  }
+catch (exception &e)
+  {
+    cerr << "Error: " << e.what() << endl;
+  }
+
+
