95668ffd8743a359a1b524f53dc74de5ec648988
m@rtin.bz <m@rtin.bz>
2022-05-28 19:19:15 -0200

    Section 14.3.2

---
+++ c14/section_14.3.2/Str_blob_ptr.cpp
@@ -0,0 +1,109 @@
+//
+// Created by martin on 24/05/2022.
+//
+
+#include "Str_blob_ptr.h"
+#include "Str_blob.h"
+
+Str_blob_ptr::Str_blob_ptr ()
+    : current(0)
+  {
+
+  }
+
+Str_blob_ptr::Str_blob_ptr (Str_blob &a, size_t sz)
+    : w_ptr(a.data),
+    current(sz)
+  {
+
+  }
+
+Str_blob_ptr &Str_blob_ptr::operator= (const Str_blob_ptr &rhs)
+  {
+    if (this == &rhs)
+      { //is there any need of self-assignment .
+        return *this;
+      }
+
+    w_ptr = rhs.w_ptr;
+    current = rhs.current;
+
+    cout << "copy assignment constructor called." << endl;
+
+    return *this;
+  };
+
+string &Str_blob_ptr::dereference () const
+  {
+//    auto p = check(current, "dereference past end");
+//    return ( *p )[current];
+
+    return ( *check(current, "dereference past end") )[current];
+  }
+
+Str_blob_ptr &Str_blob_ptr::increment ()
+  {
+    auto ret = check(current, "increment past end of Str_blob_ptr");
+    ++current;
+    return *this;
+  }
+
+shared_ptr<vector<string>> Str_blob_ptr::check (size_t i, const string &msg) const
+  {
+    auto ret = w_ptr.lock();
+    if (!ret)
+      {
+        throw runtime_error("unbound Str_blob_ptr");
+      }
+    if (i >= ret->size())
+      {
+        throw out_of_range(msg);
+      }
+    return ret;
+  }
+
+Str_blob_ptr::~Str_blob_ptr ()
+  {
+//    cout << "destroying str_blob_ptr" << endl;
+  }
+
+bool operator== (const Str_blob_ptr &lhs, const Str_blob_ptr &rhs)
+  {
+    auto lhs_ptr = lhs.w_ptr.lock();
+    auto rhs_ptr = rhs.w_ptr.lock();
+
+    if (!( lhs_ptr && rhs_ptr ))
+      {
+        throw runtime_error("weak pointer is expired");
+        return false;
+      }
+
+    return *lhs_ptr == *rhs_ptr &&
+           lhs.current == rhs.current;
+  }
+
+bool operator!= (const Str_blob_ptr &lhs, const Str_blob_ptr &rhs)
+  {
+    return !( rhs == lhs );
+  }
+
+ostream &operator<< (ostream &os, const Str_blob_ptr &ptr)
+  {
+    if (auto spt = ptr.w_ptr.lock())
+      {
+        for (auto &s : *spt)
+          {
+            cout << s;
+            if (s != spt->back())
+              {
+                cout << " ";
+              }
+          }
+      }
+    else
+      {
+        std::cout << "weak pointer is expired" << endl;
+      }
+
+    return os;
+  }
---
+++ c14/section_14.3.2/employee.h
@@ -0,0 +1,26 @@
+#pragma once
+//
+// Created by martin on 28-05-22.
+//
+#include <bits/stdc++.h>
+#include <ostream>
+
+using namespace std;
+
+struct employee
+    {
+        string name;
+        string adresse;
+        string country;
+        string phone;
+        unsigned int id;
+
+        bool operator== (const employee &rhs) const;
+
+        bool operator!= (const employee &rhs) const;
+
+    };
+
+istream &operator>> (istream &is, employee &employee);
+
+ostream &operator<< (ostream &os, const employee &employee);
