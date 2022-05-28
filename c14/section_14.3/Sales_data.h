96a45e0d9e57bb84c72937406cb5c13e00df17a8
m@rtin.bz <m@rtin.bz>
2022-05-28 16:57:02 -0200

    Section 14.3

---
+++ c14/section_14.3/Sales_data.h
@@ -0,0 +1,75 @@
+//
+// Created by martin on 28/05/2022.
+//
+
+#include <bits/stdc++.h>
+#include <ostream>
+
+using namespace std;
+
+// 0-201-78655 5 98.50
+
+class Sales_data
+   {
+   public:
+      // constructors
+
+      Sales_data ();
+
+      Sales_data (string book_number);
+
+      Sales_data (string book_number, unsigned units_sold, double price);
+
+      explicit Sales_data (istream &is);
+
+      // methods
+
+      Sales_data &combine (const Sales_data &rhs);
+
+      // getters and setters
+
+      [[nodiscard]] const string &isbn () const;
+
+      void set_isbn (const string &book_no);
+
+      [[nodiscard]] unsigned int units_sold () const;
+
+      void set_units_sold (unsigned int units_sold);
+
+      [[nodiscard]] double revenue () const;
+
+      void set_revenue (double revenue);
+
+      Sales_data &operator+= (const Sales_data &other);
+
+      friend bool operator== (const Sales_data &lhs, const Sales_data &rhs);
+
+      friend bool operator!= (const Sales_data &lhs, const Sales_data &rhs);
+
+   private:
+
+      [[nodiscard]] double avg_price () const;
+
+      std::string m_book_no;
+      unsigned m_units_sold { 0 };
+      double m_revenue { 0.0 };
+
+      // friends
+
+      friend Sales_data add (const Sales_data &lhs, const Sales_data &rhs);
+
+      friend istream &read (istream &is, Sales_data &item);
+
+      friend ostream &print (ostream &os, const Sales_data &item);
+
+   };
+
+bool operator== (const Sales_data &lhs, const Sales_data &rhs);
+
+bool operator!= (const Sales_data &lhs, const Sales_data &rhs);
+
+istream &operator>> (istream &is, Sales_data &data);
+
+ostream &operator<< (ostream &os, const Sales_data &data);
+
+Sales_data operator+ (const Sales_data &lhs, const Sales_data &rhs);
