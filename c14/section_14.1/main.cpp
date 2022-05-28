a59750b860c6f21e9cf5777a3da28be46e156c6c
m@rtin.bz <m@rtin.bz>
2022-05-28 16:23:01 -0200

    Section 14.1

--- c14/section_14.1/main.cpp
+++ c14/section_14.1/main.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include "Sales_data.h"
+#include "employee.h"
 
 using namespace std;
 
@@ -19,12 +20,26 @@ try
 //    cout << sales_data1 << endl;
 //    cout << sales_data1 + sales_data3 << endl;
 
-    Sales_data sales_data_input_test;
+//    Sales_data sales_data_input_test;
+//
+//    istringstream iss { "{ 666-XYZ-ABC 6 66.66 }" };
+//
+//    iss >> sales_data_input_test;
+//    cout << sales_data_input_test << endl;
 
-    istringstream iss { "{ 666-XYZ-ABC 6 66.66 }" };
+    employee frank {"frank", "North-pole", "Greenland", "+299-123-456", 1};
+    employee smilla {"Smilla", "South-pole", "Argentina", "+54-987-654-321", 2};
 
-    iss >> sales_data_input_test;
-    cout << sales_data_input_test << endl;
+    cout << boolalpha;
+    cout << frank << endl;
+    cout << smilla << endl;
+    cout << (frank == smilla) << endl;
+    cout << (frank != smilla) << endl;
+
+    employee mrx;
+    istringstream iss {"MrX Unknown Unknown +000-000-000 1"};
+    iss >> mrx;
+    cout << mrx;
 
     return 0;
   }
