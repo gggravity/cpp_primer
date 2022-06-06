d982f96e82ee7e306fb92d8eeb2d0dd8fce169ed
martin <m@rtin.bz>
2022-06-05 23:48:29 -0200

    section_16.2.2

---
+++ c16/section_16.2.2/main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < typename T, typename U >
+auto compare (T t, U u)
+  {
+    if (t < u)
+      {
+        return -1;
+      }
+    if (u < t)
+      {
+        return 1;
+      }
+    return 0;
+  }
+
+int main ()
+try
+  {
+    int a;
+    double b;
+    auto ret = max(a, int(b));
+
+    cout << ret << endl;
+
+    auto ptr { make_shared<int>(int { 999 }) };
+
+    cout << *ptr << endl;
+
+    auto comp { compare("string", "another") };
+
+    cout << comp << endl;
+
+    return 0;
+  }
+catch (exception &e)
+  {
+    cerr << "Error: " << e.what() << endl;
+  }
+
+
