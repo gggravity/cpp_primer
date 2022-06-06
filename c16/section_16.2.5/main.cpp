a5e5b51a2c7d641c93bb8bcfb5735d897ec2b7b8
martin <m@rtin.bz>
2022-06-06 11:37:47 -0200

    section_16.2.5

---
+++ c16/section_16.2.5/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < typename T >
+void g (T &&val)
+  {
+    cout << boolalpha;
+    cout << is_const<T>::value << " " << typeid(val).name() << endl;
+
+//    vector<T> v;
+  }
+
+
+
+int main ()
+try
+  {
+    int i { 0 };
+    const int ci { i };
+
+    g(i);
+    g(ci);
+    g(i * ci);
+
+    g(i = ci);
+
+    g(42);
+
+  }
+catch (exception &e)
+  {
+    cerr << "Error: " << e.what() << endl;
+  }
+
+
