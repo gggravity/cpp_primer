90baa9098393370557cb299d21a5e586422f3222
martin <m@rtin.bz>
2022-06-12 22:32:36 -0200

    section_18.2.4

---
+++ c18/section_18.2.4/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace primerLib
+  {
+    void compute ();
+
+    void compute (const void *)
+      {
+        cout << "void compute (const void *)" << endl;
+      }
+  }
+
+using primerLib::compute;
+
+void compute (int)
+  {
+    cout << "void compute (int)" << endl;
+  }
+
+void compute (double, double = 3.4);
+
+void compute (char *, char * = 0);
+
+void f ()
+  {
+    compute(0); // uses void compute (int);
+  }
+
+int main ()
+  {
+//    using primerLib::compute; // same
+
+    f();
+
+    return 0;
+  }
